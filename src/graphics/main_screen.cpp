70ef8b78a6b6873a9b1cb3f857bdb593dc84c3b9
zeldafan3421 <aiden.r.ross24@gmail.com>
2025-06-19 12:06:39 -0400

    c_GridSpacingc_GridSpacing

--- src/graphics/main_screen.cpp
+++ src/graphics/main_screen.cpp
@@ -13,8 +13,11 @@ void MainScreen::Update()
 
 void MainScreen::Draw()
 {
+    constexpr float c_GridSpacing = 1.0f;
+    constexpr int c_GridSlices = 10;
+
     BeginMode3D(m_Camera);
-    DrawGrid(10, 1.0f);
+    DrawGrid(c_GridSlices, c_GridSpacing);
     EndMode3D();
 }
 
